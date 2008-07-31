112e252db512ee5ed1d9e8d2a0fbc0e0f7f8bcd4
fcecagno <fcecagno@39649dc1-f352-0410-822d-0b1548283ca8>
2008-07-31 03:06:44 +0000

    similarity não tem porque solicitar os tamanhos das strings. Correção bem simples.

--- Programacao_Dinamica/Similaridade de Strings.cpp
+++ Programacao_Dinamica/Similaridade de Strings.cpp
@@ -6,15 +6,17 @@ using namespace std;
 
 /* Similaridade de Strings */
 // Função que pega o minimo
-int min(int m, int n, int o)
+int min3(int m, int n, int o)
 {
 	if((m <= n)&&(m <= o)) return m;
 	if((n <= m)&&(n <= o)) return n;
 	return o;
 }
 // Funcao que retorna a similaridade
-int similarity(string str1, string str2, int size1, int size2)
+int similarity(string str1, string str2)
 {
+    int size1 = str1.size();
+    int size2 = str2.size();
 	vector< vector<int> > matriz(size1+1, size2+1);
 	for(int i = 0; i < size1 + 1; i++) {
 		matriz[i][0] = i;
@@ -28,7 +30,7 @@ int similarity(string str1, string str2, int size1, int size2)
 			if(str1[i-1] == str2[j-1])
 				matriz[i][j] = matriz[i-1][j-1];
 			else
-				matriz[i][j] = min(matriz[i-1][j-1], matriz[i-1][j], matriz[i][j-1]) + 1;
+				matriz[i][j] = min3(matriz[i-1][j-1], matriz[i-1][j], matriz[i][j-1]) + 1;
 		}
 	}
 	return matriz[size1][size2];
